6b520487242c6720a1ee58856975bdc9d465ac04
Sarah Power <sarahjonipower0@gmail.com>
2020-11-18 21:13:34 +0000

    working fizzbuzz

--- something_else/fizzbuzz.rb
+++ something_else/fizzbuzz.rb
@@ -1,3 +1,16 @@
+def divides_by_3(number)
+  number % 3 == 0
+end
+
+
 def fizzbuzz(number)
-  "fizz"
+  if divides_by_3(number) && number % 5 == 0
+    'fizzbuzz'
+  elsif divides_by_3(number)
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,19 @@ describe "fizzbuzz" do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq "fizz"
+  end
+  it 'returns itself when passed 8' do
+    expect(fizzbuzz(8)).to eq 8
+  end
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
 end
